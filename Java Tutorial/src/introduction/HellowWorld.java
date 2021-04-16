ffe4f8b7d018b4cc93a35ca1193b31f52221ec92
abrahamgebre <abrahamgebre@gmail.com>
2021-04-15 22:45:30 -0400

    second commit

---
+++ Java Tutorial/src/introduction/HellowWorld.java
@@ -0,0 +1,18 @@
+package introduction;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		//this is my 1st Java Program
+		/*This 
+		 * is 
+		 * my 
+		 * First 
+		 * Java program 
+		 * 
+		 */
+		System.out.println("Hellow World");
+		System.out.println("Abraham G nice work")
+	}
+	
+}
